bfb58e64632516617b3cf17646f63c2c5f4deb60
Paulo Correia <paulosimoescorreia@gmail.com>
2019-02-26 18:08:08 +0000

    cards first version

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,12 +4,12 @@ class Car < ApplicationRecord
   has_many :reviews, through: :bookings
 
   validates :brand, presence: true, allow_blank: false
-  validates :model, presence: true, allow_blank: false
-  validates :car_type, presence: true, inclusion: { in: %w(small medium large) }
-  validates :fuel_type, presence: true, inclusion: { in: %w(small medium large) }
-  validates :car_photo, presence: true, allow_blank: false
-  validates :year, presence: true, allow_blank: false, numericality: { only_integer: true }
-  validates :mileage, presence: true, allow_blank: false, numericality: { only_integer: true }
+  # validates :model, presence: true, allow_blank: false
+  # validates :car_type, presence: true, inclusion: { in: %w(small medium large) }
+  # validates :fuel_type, presence: true, inclusion: { in: %w(small medium large) }
+  # validates :car_photo, presence: true, allow_blank: false
+  # validates :year, presence: true, allow_blank: false, numericality: { only_integer: true }
+  # validates :mileage, presence: true, allow_blank: false, numericality: { only_integer: true }
 
   # Add scope?
   # validates :price, presence: true, allow_blank: false, numericality: true
