96ef9cf2e48ef501f9e4591b2a554c29ebd299c4
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-02-26 16:50:50 +0000

    Gemfile updated with nokogiri; seeds created for all models (without variable test cases for bookings and ratings); added csv with seed data for users and cars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :birthdate, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birthdate, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address, :birthdate, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birthdate, :photo])
   end
 end
 
