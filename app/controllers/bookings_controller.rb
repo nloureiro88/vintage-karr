bbeab5112490c0c3b75c1acbebd97a6f273ebf2a
Nuno Loureiro <nunoloureiro88@gmail.com>
2019-02-27 01:19:15 +0000

    Added authorize sentences to ratings and bookings controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,13 @@
 class BookingsController < ApplicationController
   def new
+    authorize @booking
   end
 
   def create
+    authorize @booking
   end
 
   def destroy # only if status "created"
+    authorize @booking
   end
 end
--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,7 +1,9 @@
 class RatingsController < ApplicationController
   def new
+    authorize @rating
   end
 
   def create
+    authorize @rating
   end
 end
